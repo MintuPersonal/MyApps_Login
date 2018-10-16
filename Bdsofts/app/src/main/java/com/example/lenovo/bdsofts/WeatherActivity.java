5f67a544c699e21024a263bbb55e871bbc18491f
Mintu <gmsanzid@gmail.com>
2018-10-16 17:31:39 +0600

    API not Working

--- Bdsofts/app/src/main/java/com/example/lenovo/bdsofts/WeatherActivity.java
+++ Bdsofts/app/src/main/java/com/example/lenovo/bdsofts/WeatherActivity.java
@@ -1,18 +1,17 @@
 package com.example.lenovo.bdsofts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Method;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -25,17 +24,22 @@ public class WeatherActivity extends AppCompatActivity {
 
     public void FindWeather(){
 
-            String Url="api.openweathermap.org/data/2.5/weather?q=London";
-            JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, Url,null, new Response.Listener<JSONObject>() {
+        String Url="https://api.github.com/repos/octocat/Hello-World";
+        JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, Url,null, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
                     try {
-                        JSONArray jsonArray = response.getJSONArray("Employees");
-                        for (int i =0; i<jsonArray.length(); i++){
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            String Name = jsonObject.getString("cityName");
-                            String Location = jsonObject.getString("Location");
-                        };
+                        JSONArray jsonArray = response.getJSONArray("owner");
+                        String name = jsonArray.getString(1);
+                        TextView cityName = findViewById(R.id.tvCityName);
+                        cityName.append(name);
+//                        for (int i =0; i<jsonArray.length(); i++){
+//                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+//                            String description = jsonObject.getString("description");
+//                            String Location = jsonObject.getString("Location");
+//                           TextView cityName = findViewById(R.id.tvCityName);
+//                           cityName.append("description");
+//                        };
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
