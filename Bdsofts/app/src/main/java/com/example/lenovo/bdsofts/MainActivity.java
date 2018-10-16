24d44d6f1325d6ae1b9b62fd62edf23c6ca9306b
Mintu <gmsanzid@gmail.com>
2018-10-16 16:14:41 +0600

    Json Data Get

--- Bdsofts/app/src/main/java/com/example/lenovo/bdsofts/MainActivity.java
+++ Bdsofts/app/src/main/java/com/example/lenovo/bdsofts/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.lenovo.bdsofts;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText Name;
+    private EditText Pass;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Name =findViewById(R.id.userName);
+        Pass =findViewById(R.id.passWord);
+        Info =findViewById(R.id.infoMsg);
+        Login =findViewById(R.id.btnLogin);
 
+        Info.setText("No of Attempts remaining: 5");
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(),Pass.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String UserName, String PassWord){
+        if (UserName.toUpperCase().equals("ADMIN") && PassWord.toUpperCase().equals("12345")){
+            Activity activity =new Activity();
+            Intent intent =new Intent(MainActivity.this, WeatherActivity.class);
+            startActivity(intent);
+        }
+        else {
+            counter--;
+            Info.setText("UserName Or PassWord Invalid ! You are Clicked "+ String.valueOf(counter)+ " Time.");
+            if (counter ==0)
+            {
+                Login.setEnabled(false);
+            }
+            //Info.setText("UserName Or PassWord Invalid !");
+        }
     }
 }
---
+++ Bdsofts/app/src/main/java/com/example/lenovo/bdsofts/WeatherActivity.java
@@ -0,0 +1,59 @@
+package com.example.lenovo.bdsofts;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.jar.Attributes;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        FindWeather();
+    }
+
+    public void FindWeather(){
+
+        try
+        {
+            String Url="api.openweathermap.org/data/2.5/weather?q=London";
+            JsonObjectRequest jor = new JsonObjectRequest(Method.PUBLIC, Url,null, new Response.);
+            JsonObjectRequest joor = new JsonObjectRequest(Request.Method.GET, Url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("Employees");
+                        for (int i =0; i<jsonArray.length(); i++){
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String Name = jsonObject.getString("cityName");
+                            String Location = jsonObject.getString("Location");
+                        };
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            });
+
+        }catch (JSONException ex){
+            ex.printStackTrace();
+        }
+
+        }
+}
